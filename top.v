149a9d27f612aa356557913b83b089e7192c15cb
Salvatore Pontarelli <salvatore.pontarelli@uniroma1.it>
2023-10-19 16:09:40 +0000

    move top to sv

--- top.v
+++ top.v
@@ -1,35 +1,35 @@
 `timescale 1ns / 1ps
 
 module top(
-    input clk,              // from Basys 3
-    input reset,            // btnR
-    input KEYSIG_DATA,      // PS2
-    input KEYSIG_CLK,       // PS2
-    input [15:0] sw,              // SWITCH
+    input logic clk,              // from Basys 3
+    input logic reset,            // btnR
+    input logic KEYSIG_DATA,      // PS2
+    input logic KEYSIG_CLK,       // PS2
+    input logic [15:0] sw,              // SWITCH
     
-    input up,down,       // push buttons
+    input logic up,down,       // push buttons
     
     //LEDs
-    output [15:0] LED, 
+    output logic [15:0] LED, 
     
     //seven segment display
-    output [3:0] an,           //select digit 
-    output [7:0] seg,  // select segment    
+    output logic[3:0] an,           //select digit 
+    output logic [7:0] seg,  // select segment    
     
-    output h_sync,           // to VGA port
-    output v_sync,           // to VGA port
-    output [3:0] R_VAL,       // to DAC, to VGA port
-    output [3:0] G_VAL,       // to DAC, to VGA port
-    output [3:0] B_VAL       // to DAC, to VGA port
+    output logic h_sync,           // to VGA port
+    output logic v_sync,           // to VGA port
+    output logic [3:0] R_VAL,       // to DAC, to VGA port
+    output logic [3:0] G_VAL,       // to DAC, to VGA port
+    output logic [3:0] B_VAL       // to DAC, to VGA port
     );
     
     
-    wire w_vid_on, w_p_tick;
-    wire [9:0] w_x, w_y;
-    reg [11:0] rgb_reg;
-    wire [11:0] rgb_next;
-    wire [7:0] DATA,asciiOUT;
-    wire HZ5_CLK, NewData, KEYPRESS_S, KEYPRESS_P, KEYPRESS_R, KEYPRESS_ESC,KEYPRESS_UP, KEYPRESS_DOWN, KEYPRESS_LEFT, KEYPRESS_RIGHT;
+    logic w_vid_on, w_p_tick;
+    logic [9:0] w_x, w_y;
+    logic [11:0] rgb_reg;
+    logic [11:0] rgb_next;
+    logic [7:0] DATA,asciiOUT;
+    logic HZ5_CLK, NewData, KEYPRESS_S, KEYPRESS_P, KEYPRESS_R, KEYPRESS_ESC,KEYPRESS_UP, KEYPRESS_DOWN, KEYPRESS_LEFT, KEYPRESS_RIGHT;
     
     assign LED[15:3] = sw[15:3];
     assign LED[2] = sw[0] & sw[1];
