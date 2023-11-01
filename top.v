0a62fb7845306efb77b11e5404d7d866f2e110dc
Salvatore Pontarelli <salvatore.pontarelli@uniroma1.it>
2023-11-01 15:17:28 +0000

    update top to drive 4 digits

--- top.v
+++ top.v
@@ -22,8 +22,34 @@ module top(
     output logic [3:0] G_VAL,       // to DAC, to VGA port
     output logic [3:0] B_VAL       // to DAC, to VGA port
     );
+
     
-    
+    logic [63:0] counter;
+    logic [3:0] cifra;
+    logic [1:0] sel;
+
+    always_ff @(posedge clk) begin
+        if (reset==1) counter=0;
+        else counter = counter +1;
+    end
+
+    assign LED[15:3] = sw[15:3];
+    assign LED[2] = sw[0] & sw[1];
+    assign LED[1] = sw[0] | sw[1];
+    assign LED[0] = sw[0] ^ sw[1];
+
+    assign sel= counter[20:19];
+    assign {cifra,an}= (sel==2'b00) ? {counter[26:23], 4'b0111 }:
+                       (sel==2'b01) ? {counter[30:27], 4'b1011}:
+                       (sel==2'b10) ? {counter[34:31], 4'b1101}:
+                                      {counter[38:35], 4'b1110};
+
+    seven_segment_sw ss(.sw(cifra), .an(), .ca(seg[0]), .cb(seg[1]), .cc(seg[2]), .cd(seg[3]), .ce(seg[4]), .cf(seg[5]), .cg(seg[6]));
+
+
+
+/*
+
     logic w_vid_on, w_p_tick;
     logic [9:0] w_x, w_y;
     logic [11:0] rgb_reg;
@@ -36,24 +62,24 @@ module top(
     assign LED[1] = sw[0] | sw[1];
     assign LED[0] = sw[0] ^ sw[1];
     
-    //seven_segment ss(.clk(clk), .reset(reset), .key(asciiOUT), .an(an), .ca(seg[0]), .cb(seg[1]), .cc(seg[2]), .cd(seg[3]), .ce(seg[4]), .cf(seg[5]), .cg(seg[6]));
-    seven_segment_sw ss(.sw(sw), .an(an), .ca(seg[0]), .cb(seg[1]), .cc(seg[2]), .cd(seg[3]), .ce(seg[4]), .cf(seg[5]), .cg(seg[6]));
+    seven_segment ss(.clk(clk), .reset(reset), .key(asciiOUT), .an(an), .ca(seg[0]), .cb(seg[1]), .cc(seg[2]), .cd(seg[3]), .ce(seg[4]), .cf(seg[5]), .cg(seg[6]));
     
     /*vga_controller vga(.clk_100MHz(clk), .reset(reset), .video_on(w_vid_on),
                        .hsync(h_sync), .vsync(v_sync), .p_tick(w_p_tick), .x(w_x), .y(w_y));
     pixel_gen pg(.clk(clk), .reset(reset), .up(KEYPRESS_UP), .down(KEYPRESS_DOWN), 
                  .video_on(w_vid_on), .x(w_x), .y(w_y), .rgb(rgb_next));
     
-    PS2Controller KeyboardDriver(KEYSIG_CLK, KEYSIG_DATA, DATA, asciiOUT, NewData, KEYPRESS_S,
-    KEYPRESS_P, KEYPRESS_R, KEYPRESS_ESC,KEYPRESS_UP, KEYPRESS_DOWN, KEYPRESS_LEFT, KEYPRESS_RIGHT);
     
     */
+/*    PS2Controller KeyboardDriver(KEYSIG_CLK, KEYSIG_DATA, DATA, asciiOUT, NewData, KEYPRESS_S,
+    KEYPRESS_P, KEYPRESS_R, KEYPRESS_ESC,KEYPRESS_UP, KEYPRESS_DOWN, KEYPRESS_LEFT, KEYPRESS_RIGHT);
     
     // rgb buffer
-    /*always @(posedge clk)
+    always @(posedge clk)
         if(w_p_tick)
             rgb_reg <= rgb_next;
             
     assign {R_VAL,G_VAL,B_VAL} = rgb_reg;
-    */
+    
+*/
 endmodule
